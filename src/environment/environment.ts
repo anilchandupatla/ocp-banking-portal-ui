a397c03057b575115f5d6ec2b543275a73664f5d
anilchandupatla <anil.chandupatla@outlook.com>
2025-02-11 07:07:03 +0530

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api',
-    tokenName :  "authToken",
-    origin:'http://localhost:4200' 
-  };
+  production: true,
+  apiUrl: 'http://ostbapi-vijaysimha-dev.apps.rm3.7wse.p1.openshiftapps.com:8180',
+  tokenName :  "sha256~YBFM_DuM5OXa-d0NeAE-QzPzvPRbaedZZzI6afdi8zM",
+  origin:'http://ostbapi-vijaysimha-dev.apps.rm3.7wse.p1.openshiftapps.com:8180' 
+};
